bc3a9d404c5a3f4e64abc347c25891a6b0d26820
“CGOME”59 <cgome59@bancodebogota.com.co>
2026-07-22 15:37:33 -0500

    feat(arch): update routes with shell

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,50 +1,49 @@
 import { Routes } from '@angular/router';
 
+import { Shell } from './shell/shell/shell';
+
 import { DashboardComponent } from './microfrontends/dashboard-mf/dashboard/dashboard';
 import { BookingComponent } from './microfrontends/booking-mf/booking/booking';
 import { HistoryComponent } from './microfrontends/history-mf/history/history';
 import { ProfileComponent } from './microfrontends/profile-mf/profile/profile';
 import { LoginComponent } from './microfrontends/auth-mf/login/login';
 
-import { TutorsComponent } from './pages/tutors/tutors';
-
 import { AuthGuard } from './core/services/auth.guard';
 
 export const routes: Routes = [
 
   {
-    path: '',
-    component: DashboardComponent,
-    canActivate: [AuthGuard]
-  },
-
-  {
-    path: 'booking',
-    component: BookingComponent,
-    canActivate: [AuthGuard]
-  },
-
-  {
-    path: 'history',
-    component: HistoryComponent,
-    canActivate: [AuthGuard]
-  },
-
-  {
-    path: 'profile',
-    component: ProfileComponent,
-    canActivate: [AuthGuard]
-  },
-
-  {
-    path: 'tutors',
-    component: TutorsComponent,
-    canActivate: [AuthGuard]
-  },
-
-  {
     path: 'login',
     component: LoginComponent
+  },
+
+  {
+    path: '',
+    component: Shell,
+    canActivate: [AuthGuard],
+    children: [
+
+      {
+        path: '',
+        component: DashboardComponent
+      },
+
+      {
+        path: 'booking',
+        component: BookingComponent
+      },
+
+      {
+        path: 'history',
+        component: HistoryComponent
+      },
+
+      {
+        path: 'profile',
+        component: ProfileComponent
+      }
+
+    ]
   }
 
 ];
