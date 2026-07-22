b9f51be7cf9a2db951cbe126078521e9cd361011
“CGOME”59 <cgome59@bancodebogota.com.co>
2026-07-22 12:17:38 -0500

    init proyect microfrontends

--- src/app/shared/card-tutor/card-tutor.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-card-tutor',
-  imports: [],
-  templateUrl: './card-tutor.html',
-  styleUrl: './card-tutor.css',
-})
-export class CardTutor {}
